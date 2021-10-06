d0b9438de785ec05deb3f41226e0566fb5f714d6
imanshafiei <imanshafiei@ymail.com>
2021-10-06 23:21:15 +0330

    correct image meta add.

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
             },
             {
                 property: 'og:image',
-                content: '/imanshafiei.jpeg'
+                content: 'https://imanshafiei540.github.io/img/imanshafiei.jpeg'
             },
         ],
         link: [{
